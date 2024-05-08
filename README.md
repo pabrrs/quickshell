eb9f246a0a4fa9066ba6fca2f85fcbb15c467b29
pabrrs <paulo.alfredo.barros@gmail.com>
2024-05-07 20:35:22 -0400

    feat: init

---
+++ README.md
@@ -0,0 +1,20 @@
+# ⚡️ quickshell ⚡️
+
+Fast select day-to-day commands, because ⏰ is 💰!
+
+![docs](./docs/quickshell.png)
+
+## Getting started
+
+```sh
+# create a link to script
+chmod +x quickshell.sh
+
+# setup your commands
+mv commands.txt.tmpl commants.txt
+echo "echo \"my nice command\"" > commands.txt
+echo "echo \"my other nice command\"" > commands.txt
+
+# run and have fun
+./quickshell
+```
---
+++ quickshell.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+quickshell_label() {
+    echo "⚡️ quickshell ⚡️"
+}
+
+# Function to clear the screen
+clear_screen() {
+    printf "\033c"
+}
+
+# Function to display the dropdown list
+display_dropdown() {
+    clear_screen
+    quickshell_label
+    echo "Select a command:"
+    for ((i=0; i<${#options[@]}; i++)); do
+        if [ $i -eq $selected_index ]; then
+            echo "> $(tput bold)${options[$i]}$(tput sgr0)"
+        else
+            echo "  ${options[$i]}"
+        fi
+    done
+}
+
+# Function to handle arrow key navigation
+handle_input() {
+    read -rsn1 key
+    case $key in
+        A)  # Up arrow
+            ((selected_index--))
+            ;;
+        B)  # Down arrow
+            ((selected_index++))
+            ;;
+        "") # Enter key
+            clear_screen
+            quickshell_label
+            echo "Running ${options[$selected_index]}"
+            echo ""
+            eval "${options[$selected_index]}" 
+            exit 0
+            ;;
+    esac
+
+    # Ensure selected index stays within bounds
+    if [ $selected_index -lt 0 ]; then
+        selected_index=$((${#options[@]} - 1))
+    elif [ $selected_index -ge ${#options[@]} ]; then
+        selected_index=0
+    fi
+}
+
+# Read options from file and populate the array
+options=()
+while IFS= read -r line; do
+    options+=("$line")
+done < "$PWD/commands.txt"
+
+# Initialize selected index
+selected_index=0
+
+# Main loop
+while true; do
+    display_dropdown
+    handle_input
+done
